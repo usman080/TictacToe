c4729f61329cb6b3cd1b2cb0c3e8d3ab77ef3c34
usman080 <66534063+usman080@users.noreply.github.com>
2022-03-26 16:07:15 +0530

    Update

--- my1stprojecttictactoe.c
+++ my1stprojecttictactoe.c
@@ -12,20 +12,8 @@
     int i, j;
     char usm;
     display();
-    /*int position[3][3]={'1','2','3','4','5','6','7','8','9'};
-     printf("The positions in Board for playing the game:\n");
-     //dispalying board position
-     for(i=0;i<3;i++)
-     {
-         for(j=0;j<3;j++){
-     printf("%d |" , &position[i][j]);
-         }
-         printf("\n");
-     }*/
-     printf("Enter player1 name:");
-     //gets(f);//scanf("%s\n",&f);
-     printf("Enter player2 name:");
-     //gets(m);//scanf("%s\n",&m);
+    
+     
      input();//starting the game
      usm = check();//displaying the winner
      if(usm=='X')
@@ -138,30 +126,7 @@ void display()//displaying the game
         }
         printf("\n");
     }
-}/*
-char check()//checking the winning possibilities
-{
-     int i;
-      char uk = '_';
-    for(i=0;i<=2;i++)
-    {
-        if(board[i][0]==board[i][1] && board[i][0]==board[i][2]) //for winning possibility in rows
-        {
-            uk = board[i][0];
-        }
-
-        else if(board[0][i]==board[1][i] && board[0][i]==board[2][i]) //for winning possibility in columns
-        {
-            uk = board[0][i];
-        }
-    }//for winning possibilities in diagonals
-    if(board[0][0]==board[1][1]&&board[1][1]==board[2][2])
-        uk = board[0][0];
-    if(board[0][2]==board[1][1]&&board[1][1]==board[2][0])
-        uk = board[1][1];
-
-    return uk ;
-}*/
+}
 char check(void)
 {
    int i;
